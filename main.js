0eff957f4e5272de1a738f1cccfcb96ad7e23a28
HoMaiQue <homaique.it@gmail.com>
2021-07-10 00:06:03 +0700

    additonal

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ dropdownSubMenus.forEach((dropdownSubMenu, index) => {
     })
 })
 
-
 const dropMenuItems = document.querySelectorAll('.dropdown-menu__item');
 const dropMenuLinks = document.querySelectorAll('.dropdown-menu__link');
 const underlineNavColor = document.querySelectorAll('.underline-nav-color')
@@ -31,10 +30,7 @@ dropMenuItems.forEach((item, index) => {
 
     })
 })
-
-
 //--------------------
-
 const slideBigImgs = document.getElementById('slide-big-img');
 const slideSmallImgs = document.getElementById('slide-small-img');
 
@@ -44,10 +40,8 @@ const rightBtn = document.querySelector('.slider__button-right');
 const slideBigImg = document.querySelectorAll('#slide-big-img .slider__img-big');
 const slideSmallImg = document.querySelectorAll('#slide-small-img img');
 const slideLogo = document.querySelectorAll('.slider__logo');
-console.log(slideLogo);
 
 let index = 0;
-
 let interval = setInterval(run, 5000);
 
 function run() {
@@ -56,17 +50,29 @@ function run() {
 }
 
 function changeSlide() {
+    const windowWidth = window.innerWidth;
     if (index > slideBigImg.length - 1) {
         index = 0;
     } else if (index < 0) {
         index = slideBigImg.length - 1;
     }
+
+
     removeOpacity(slideBigImg);
     removeOpacity(slideSmallImg);
     slideBigImg[index].classList.add('active');
     slideSmallImg[index].classList.add('active');
-    slideBigImgs.style.transform = `translateX(${-index * 723}px)`;
-    slideSmallImgs.style.transform = `translateX(${-index * 349}px)`;
+    if (windowWidth >= 740 && windowWidth <= 1023) {
+        slideBigImgs.style.transform = `translateX(${-index * 384}px)`;
+        slideSmallImgs.style.transform = `translateX(${-index * 186}px)`;
+    } else if (windowWidth <= 739) {
+        slideBigImgs.style.transform = `translateX(${-index * 300}px)`;
+        slideSmallImgs.style.transform = `translateX(${-index * 300}px)`;
+    } else {
+        slideBigImgs.style.transform = `translateX(${-index * 723}px)`;
+        slideSmallImgs.style.transform = `translateX(${-index * 349}px)`;
+    }
+
     removeOpacity(slideLogo);
     slideLogo[index].classList.add('active');
 
@@ -82,7 +88,6 @@ function resetInterval() {
     clearInterval(interval);
 
     interval = setInterval(run, 5000);
-
 }
 
 rightBtn.addEventListener('click', (e) => {
@@ -91,7 +96,6 @@ rightBtn.addEventListener('click', (e) => {
     resetInterval();
     changeSlide();
 })
-
 
 leftBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -114,16 +118,66 @@ function setIntervalPercent(realWidth, chart) {
             clearInterval(interval);
         }
     }, 30);
-
-
 }
 
 const percentCountry = 75;
 const percentVineYard = 92;
 const percentWineTasting = 86;
-setIntervalPercent(percentCountry, chartCountry);
-setIntervalPercent(percentVineYard, chartVineYard);
-setIntervalPercent(percentWineTasting, chartWineTasting);
+
+function runPercent() {
+    let pageOffset = pageYOffset;
+    if (pageOffset >= 500) {
+        setIntervalPercent(percentCountry, chartCountry);
+        setIntervalPercent(percentVineYard, chartVineYard);
+        setIntervalPercent(percentWineTasting, chartWineTasting);
+        window.removeEventListener('scroll', runPercent)
+    }
+}
+window.addEventListener('scroll', runPercent);
+
+function setIntervalNumber(realNumber, detailNumber, numberDistance) {
+    let initNumber = +detailNumber.innerText;
+    let interval = setInterval(() => {
+        initNumber += numberDistance;
+        detailNumber.innerText = initNumber;
+        if (initNumber === realNumber) {
+            clearInterval(interval);
+        }
+    }, 100)
+}
+
+function runNumber() {
+    const numberGlassOfWine = document.querySelector('.glass-of-wine');
+    const numberYearOfExperience = document.querySelector('.year-of-experience');
+    const numberUniqueWine = document.querySelector('.unique-wines');
+    const numberSortsOfWine = document.querySelector('.sorts-of-wine');
+    const pageOffset = pageYOffset;
+    console.log(pageOffset);
+    if (pageOffset >= 2390) {
+        setIntervalNumber(45, numberGlassOfWine, 3);
+        setIntervalNumber(126, numberYearOfExperience, 8);
+        setIntervalNumber(452, numberUniqueWine, 30);
+        setIntervalNumber(247, numberSortsOfWine, 16);
+        window.removeEventListener('scroll', runNumber);
+    }
+}
+
+window.addEventListener('scroll', runNumber);
+
+// 
+
+const barButton = document.querySelector('.bar-menu');
+
+const nav = document.querySelector('.nav');
+barButton.addEventListener('click', () => {
+    nav.classList.toggle('active');
+
+})
+
+
+
+
+
 
 
 
